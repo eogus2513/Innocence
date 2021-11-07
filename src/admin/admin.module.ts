ada97169268c0242300ffd830f019a8d50686bdf
정대현 <eogus_2513@dsm.hs.kr>
2021-11-07 20:24:03 +0900

    [FIX]

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,10 +3,19 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin } from '../entities/admin.entity';
+import { AccessStrategy } from 'src/stratege/jwt-access.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Admin])],
+  imports: [
+    TypeOrmModule.forFeature([Admin]),
+    JwtModule.register({
+      secret: process.env.ACCESS_JWT,
+    }),
+  ],
   controllers: [AdminController],
-  providers: [AdminService]
+  providers: [AdminService, AccessStrategy],
 })
 export class AdminModule {}
