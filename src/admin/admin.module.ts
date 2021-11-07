5e1bd611f1c629b88da7fcf4844f54bf54128cd9
정대현 <eogus_2513@dsm.hs.kr>
2021-11-08 08:28:39 +0900

    [ADD] Redis

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,11 +7,27 @@ import { AccessStrategy } from 'src/stratege/jwt-access.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import * as dotenv from 'dotenv';
 import { RefreshStrategy } from '../stratege/jwt-refresh.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 
 dotenv.config();
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Admin]), JwtModule.register({})],
+  imports: [
+    TypeOrmModule.forFeature([Admin]),
+    JwtModule.register({}),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        isGlobal: true,
+        store: redisStore,
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        ttl: 0,
+      }),
+    }),
+  ],
   controllers: [AdminController],
   providers: [AdminService, AccessStrategy, RefreshStrategy],
 })
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CACHE_MANAGER,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Admin } from '../entities/admin.entity';
 import { Repository } from 'typeorm';
@@ -7,26 +12,27 @@ import { compare, hash } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import * as moment from 'moment';
 import { SignUpRequest } from './dto/request/SignUpRequest.dto';
+import { TokenResponse } from './dto/response/TokenResponse.dto';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AdminService {
   constructor(
     @InjectRepository(Admin) private adminRepository: Repository<Admin>,
     private readonly jwtService: JwtService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
-  async Login(body: LoginRequest) {
+  async Login(body: LoginRequest): Promise<TokenResponse> {
     const user = await this.adminRepository.findOne({ id: body.id });
 
     if (!user) {
       throw new BadRequestException('User Not Exist!');
     }
-
     if (!(await compare(body.password, user.password))) {
       throw new BadRequestException('Password mismatch!');
     }
-
-    const accessToken = await this.jwtService.signAsync(
+    const access_token = await this.jwtService.signAsync(
       {
         id: body.id,
         access_exp: moment().hour(2).format('MM/DD/HH'),
@@ -36,7 +42,7 @@ export class AdminService {
         expiresIn: `${process.env.ACCESS_EXP}s`,
       },
     );
-    const refreshToken = await this.jwtService.signAsync(
+    const refresh_token = await this.jwtService.signAsync(
       {
         id: body.id,
         refresh_exp: moment().day(14).format('YYYY/MM/DD'),
@@ -46,8 +52,8 @@ export class AdminService {
         expiresIn: `${process.env.REFRESH_EXP}s`,
       },
     );
-
-    return { accessToken, refreshToken };
+    await this.cacheManager.set(body.id, refresh_token);
+    return { access_token, refresh_token };
   }
 
   async SignUp(body: SignUpRequest): Promise<void> {
