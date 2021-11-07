ff9bf77889ee8c831e4f1f62e7ed7a79f83f51d2
정대현 <eogus_2513@dsm.hs.kr>
2021-11-07 23:17:31 +0900

    [ADD] signup logic

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { LoginRequest } from './dto/request/loginRequest.dto';
+import { SignUpRequest } from './dto/request/SignUpRequest.dto';
+import { TokenResponse } from './dto/response/TokenResponse.dto';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
   @Post('login')
-  public async Login(@Body() body: LoginRequest) {
+  public async Login(@Body() body: LoginRequest): Promise<TokenResponse> {
     return await this.adminService.Login(body);
+  }
+
+  @Post('signup')
+  public async SignUp(@Body() body: SignUpRequest): Promise<void> {
+    await this.adminService.SignUp(body);
   }
 }
