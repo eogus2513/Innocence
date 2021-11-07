827644b55a4a18733dfaa0109a124cd48c10e96d
정대현 <eogus_2513@dsm.hs.kr>
2021-11-07 20:23:44 +0900

    [ADD] jwt strategy

---
+++ src/stratege/jwt-access.strategy.ts
@@ -0,0 +1,22 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+@Injectable()
+export class AccessStrategy extends PassportStrategy(Strategy, 'access') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.ACCESS_JWT,
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      id: payload.id,
+    };
+  }
+}
