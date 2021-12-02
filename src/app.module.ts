d16ac09ef5e94d62f6cd516fc9f160b1999f55b1
정대현 <eogus_2513@dsm.hs.kr>
2021-12-02 16:04:22 +0900

    [FIX] DATABASE 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
-import { DatabaseModule } from './database/database.module';
 import { AdminModule } from './admin/admin.module';
 import { UserModule } from './user/user.module';
 import { VideoModule } from './video/video.module';
 import { TitleModule } from './title/title.module';
+import { TypeormConfigModule } from './typeorm/typeorm-config.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: Joi.object({
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.number().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_DATABASE: Joi.string().required(),
-      }),
-    }),
-    DatabaseModule,
+    TypeormConfigModule,
     AdminModule,
     UserModule,
     VideoModule,
---
+++ src/typeorm/ormconfig.ts
@@ -0,0 +1,27 @@
+import { ConnectionOptions } from 'typeorm';
+import 'dotenv/config';
+import { Admin } from '../admin/entity/admin.entity';
+import { User } from '../user/entity/user.entity';
+import { Category } from '../title/entity/category.entity';
+import { Subject } from '../title/entity/subject.entity';
+import { Title } from '../title/entity/title.entity';
+import { Video } from '../video/entity/video.entity';
+
+interface DBConnectionOptions {
+  [env: string]: ConnectionOptions;
+}
+
+const connectionOptions: DBConnectionOptions = {
+  tby: {
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: false,
+    entities: [Admin, User, Category, Subject, Title, Video],
+  },
+};
+
+export { connectionOptions };
